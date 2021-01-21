2893865b6478609cbf3c294306172f87495e4dff
maivanthanh <maivanthanh.6898@gmail.com>
2021-01-21 23:14:44 +0700

    Init project and create login form

---
+++ QLBHCC/LoginForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace QLBHCC
+{
+    public partial class LoginForm : Form
+    {
+        private string connString = ConfigurationManager.ConnectionStrings["connectionString"].ToString();
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connString))
+            {
+                con.Open();
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = con;
+                comm.CommandText = "SP_Login";
+                comm.CommandType = CommandType.StoredProcedure;
+                comm.Parameters.AddWithValue("@username", tbUsername.Text);
+                comm.Parameters.AddWithValue("@password", tbPassword.Text);
+                var reader = comm.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    this.Hide();
+                    QuanLyCayCanh qlcc = new QuanLyCayCanh(tbUsername.Text);
+                    qlcc.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Thông tin đăng nhập không chính xác hoặc không tồn tại, vui lòng kiểm tra lại thông tin", "Đăng nhập thất bại", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    tbUsername.Focus();
+                }
+            }
+        }
+    }
+}
---
+++ QLBHCC/SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿Create Proc SP_Login
+@username Nvarchar(255),
+@password Nvarchar(255)
+as begin
+select * from tbl_nhanvien where sUsername = @username AND sPassword = @password
+end
+
+SP_Login "tien123","123456"
